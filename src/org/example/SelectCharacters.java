2d33bff96ef08b287edbffb7be436fa60af7747f
Eliya25 <eliyacohen2019@gmail.com>
2024-09-29 16:36:39 +0300

    +Eliya

--- src/org/example/SelectCharacters.java
+++ src/org/example/SelectCharacters.java
@@ -13,10 +13,31 @@ public class SelectCharacters extends JPanel {
     private final int width = 330;
     private final int height = 340;
 
+    private JButton selectCharacter1;
+    private JButton selectCharacter2;
+    private JButton selectCharacter3;
+
     public SelectCharacters() {
+        setLayout(null);
         this.character1 = new ImageIcon("src/main/resource/first-character.png").getImage();
         this.character2 = new ImageIcon("src/main/resource/second-character.png").getImage();
         this.character3 = new ImageIcon("src/main/resource/third-character.png").getImage();
+
+        selectCharacter1 = new JButton("Select Character 1");
+        selectCharacter1.setBounds(50,400,150,30);
+        this.add(selectCharacter1);
+
+        selectCharacter2 = new JButton("Select Character 2");
+        selectCharacter2.setBounds(50,400,150,30);
+        this.add(selectCharacter2);
+
+        selectCharacter3 = new JButton("Select Character 3");
+        selectCharacter3.setBounds(50,400,150,30);
+        this.add(selectCharacter3);
+
+        selectCharacter1.addActionListener(e -> setChosenIndexCharacter(1));
+        selectCharacter2.addActionListener(e -> setChosenIndexCharacter(2));
+        selectCharacter3.addActionListener(e -> setChosenIndexCharacter(3));
     }
 
     @Override
